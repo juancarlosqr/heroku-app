70556898d3173807c570f07a062bc914d6dde8b5
Juan Carlos Quintero <jcquintero.ce@gmail.com>
2014-08-19 00:56:57 -0430

    new skills section and css improvements

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
 <?php
+// http://designmodo.github.io/Flat-UI/
+// http://bootswatch.com/flatly/
+// http://astronautweb.co/snippet/font-awesome/
+// http://juancarlosqr.dev/index.lovin.bak.html
+// http://juancarlosqr.dev/
 include_once 'app.html';
